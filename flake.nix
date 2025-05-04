1e78135770f3710c7adb615e0d2f067ce54ed4c2
blackzeshi <sergey_zhuzhgov@mail.ru>
2025-05-04 19:11:57 +0500

    flake

--- flake.nix
+++ flake.nix
@@ -4,15 +4,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    home-manager,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
@@ -36,51 +33,13 @@
         };
 
         default = pkgs.symlinkJoin {
-          name = "zeshifyd-tools";
+          name = "zeshify-tools";
           paths = [zeshifyd zeshifyctl];
         };
       };
 
       devShells.default = pkgs.mkShell {
         buildInputs = [pkgs.go pkgs.gopls];
-      };
-
-      # ✅ home-manager module with systemd service
-      homeConfigurations = {
-        nixeshi = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-
-          modules = [
-            {
-              home.username = "nixeshi";
-              home.homeDirectory = "/home/nixeshi";
-
-              home.packages = [
-                zeshifyd
-                zeshifyctl
-              ];
-
-              systemd.user.services.zeshifyd = {
-                Unit = {
-                  Description = "Zeshifyd Notification Daemon";
-                  After = ["graphical-session.target"];
-                };
-
-                Service = {
-                  ExecStart = "${zeshifyd}/bin/zeshifyd";
-                  Restart = "on-failure";
-                  RestartSec = 1;
-                };
-
-                Install = {
-                  WantedBy = ["default.target"];
-                };
-              };
-
-              home.stateVersion = "23.11";
-            }
-          ];
-        };
       };
     });
 }
